3bc2c1010a3a6013e5f9e9cb69428b92f7b230b1
Alexandra Bledea <bledea_alexandra@yahoo.com>
2025-02-07 03:15:56 +0200

    Notification service changes

--- notification/src/main/java/com/ubbcluj/notification/listener/EmailNotificationListener.java
+++ notification/src/main/java/com/ubbcluj/notification/listener/EmailNotificationListener.java
@@ -1,6 +1,5 @@
 package com.ubbcluj.notification.listener;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ubbcluj.notification.dto.EmailNotificationDto;
 import com.ubbcluj.notification.service.EmailNotificationService;
 import lombok.extern.slf4j.Slf4j;
@@ -18,14 +17,7 @@ public class EmailNotificationListener {
     }
 
     @RabbitListener(queues = "${rabbit.queue-name}")
-    public void receiveMessage(EmailNotificationDto message) throws JsonProcessingException {
-//        EmailNotificationDto emailNotificationDto = mapper.readValue(message, EmailNotificationDto.class);
-        System.out.println("Am primit mesajul!!!!!!!");
-        log.info("Am primit mesajul ca log!!!! yay");
-        System.out.println("Received message: " + message);
-        System.out.println(message.email());
-        System.out.println(message.description());
+    public void receiveMessage(EmailNotificationDto message) {
         this.emailNotificationService.sendNotificationEmail(message.email(), message.description());
-        //TODO Add email send
     }
 }
